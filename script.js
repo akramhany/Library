45d1fcc6400bb30f128a81b3888e4ed0cc01fba8
akram <akramhany65@gmail.com>
2023-10-28 12:12:42 +0200

    Implementing most of the logic and structuring the page to match it

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const myLibrary = [];
 
-function Book(author, title, numberOfPages, readOrNot) {
+function Book(author, title, numberOfPages) {
     this.author = author;
     this.title = title;
     this.numberOfPages = numberOfPages;
-    this.readOrNot = readOrNot;
 }
 
-function addBookToLibrary(author, title, numberOfPages, readOrNot) {
-    const newBook = new Book(author, title, numberOfPages, readOrNot);
+function displayNewBook(book) {
+    const booksGrid = document.querySelector('.books-grid');
+    
+    const newBook = document.createElement('div');
+    newBook.className = "book";
+
+    for (const key in book) {
+        const newCell = document.createElement('p');
+        newCell.textContent = book[key];
+        newBook.appendChild(newCell);
+    }
+    
+    booksGrid.appendChild(newBook);
+}
+
+function addBookToLibrary(author, title, numberOfPages) {
+    const newBook = new Book(author, title, numberOfPages);
     myLibrary.push(newBook);
+    displayNewBook(newBook);
 }
 
 const addNewBookButton = document.querySelector('.addNewBook');
-const closeButton = document.querySelector('.closeButton')
+const closeButton = document.querySelector('.closeButton');
+const modalAddButton = document.querySelector('.modalAddButton')
 const modalBox = document.querySelector('#modal');
 
 
@@ -23,5 +39,22 @@ addNewBookButton.addEventListener('click', () => {
 
 closeButton.addEventListener('click', () => {
     modalBox.close();
+})
+
+modalAddButton.addEventListener('click', () => {
+    modalBox.close();
+})
+
+const formSubmit = document.querySelector('.modal-form');
+
+formSubmit.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const author = event.currentTarget.author.value;
+    const title = event.currentTarget.title.value;
+    const numberOfPages = event.currentTarget.numOfPages.value;
+    addBookToLibrary(author, title, numberOfPages);
+    event.currentTarget.author.value = "";
+    event.currentTarget.title.value = "";
+    event.currentTarget.numOfPages.value = "";
 })
 
